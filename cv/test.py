1c233e633cf63ec465124a5439fb6cf666f2cabf
Gerard <gerardlke7@gmail.com>
2025-03-22 12:15:59 +0800

    created cv pipeline: New main.py, accident_detection.py, vehicle_detection.py with basic functionality

--- cv/test.py
+++ cv/test.py
@@ -1,13 +1,24 @@
 import requests
+import cv2
+import numpy as np
 
 url = "https://api.data.gov.sg/v1/transport/traffic-images"
 
 response = requests.get(url).json()
 
-timestamp, cameras = response['items'][0]['timestamp'], response['items'][0]['cameras']
-print('current timestamp', timestamp)
+acquisition_timestamp, cameras = response['items'][0]['timestamp'], response['items'][0]['cameras']
+print('acquisition timestamp', acquisition_timestamp)
 
 for idx, camera in enumerate(cameras):
     print(idx, camera)
+    response = requests.get(camera['image'])
 
+    # img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+    img_array = np.frombuffer(response.content, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    print(img.shape)
+    # cv2.imshow('img', img)
+    # cv2.waitKey()
+
+cv2.destroyAllWindows
 # print(response.json())
